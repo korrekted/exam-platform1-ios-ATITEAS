3f5c63e269456633e3158011843ceb4d60017908
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-03-20 17:38:13 +0300

    copy nursing

---
+++ ATITEAS/Resource/ATITEAS-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ATITEAS-Thermo-Bridging-Header.h
+//  ATITEAS
+//
+//  Created by Andrey Chernyshev on 20.03.2021.
+//
+
+#ifndef ATITEAS_Thermo_Bridging_Header_h
+#define ATITEAS_Thermo_Bridging_Header_h
+
+#import <RushSDK/RushSDK.h>
+
+#endif /* ATITEAS_Thermo_Bridging_Header_h */
