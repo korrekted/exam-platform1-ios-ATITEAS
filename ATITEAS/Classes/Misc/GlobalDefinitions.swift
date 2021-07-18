238598d26cbbf349ed646c6474acdaf396d60592
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-07-18 11:19:41 +0300

    build 6

--- ATITEAS/Classes/Misc/GlobalDefinitions.swift
+++ ATITEAS/Classes/Misc/GlobalDefinitions.swift
@@ -7,17 +7,17 @@
 
 final class GlobalDefinitions {
     static let domainUrl = "https://exams.korrekted.com"
-    static let apiKey = "cJTI3sIiSExhjG90"
+    static let apiKey = "rJWkkAZ7vUNVNqpo"
 
     static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "WIJ4ZZqtgxDOHPqR"
+    static let sdkApiKey = "oEoXlqtkstZKKcpo"
 
-    static let applicationTag = "EXAM_FNP"
+    static let applicationTag = "EXAM_ATI_TEAS"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
-    static let appleAppID = "1572754729"
+    static let appleAppID = "1556998922"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
     static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
