5cd39679eac46a40d46d61958d6b550c655e0ffe
akonst17 <akonst17@gmail.com>
2022-08-01 11:05:34 +0300

    обновления Nursing; build 20

--- ATITEAS/Classes/Misc/Appearance.swift
+++ ATITEAS/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 252, green: 249, blue: 249)
-    
     static let mainColor = UIColor(integralRed: 237, green: 86, blue: 114)
     static let mainColorAlpha = UIColor(integralRed: 237, green: 86, blue: 114, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 117, green: 204, blue: 144)
-    
-    static let progress1Color = UIColor(integralRed: 237, green: 86, blue: 114)
-    static let progress2Color = UIColor(integralRed: 248, green: 205, blue: 70)
-    static let progress3Color = UIColor(integralRed: 117, green: 204, blue: 144)
-    
+    static let secondColor = UIColor(integralRed: 248, green: 205, blue: 70)
+    static let thirdColor = UIColor(integralRed: 117, green: 204, blue: 144)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 252, green: 249, blue: 249)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
